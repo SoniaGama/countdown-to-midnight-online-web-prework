3ec27a26f4cc9d2a245f2e22ccc507f4834107e8
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 19:43:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,12 +9,5 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-counter = 0
-while counter < 20
-  puts "The current number is less than 20."
-  counter += 1
-end
-
-
 def countdown_with_sleep
 end 
