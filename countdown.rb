090727c3fbe4b07f9d54f847ed5bf84d346d92d0
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 19:46:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,12 @@ end
 
 #sleep(1)
 def countdown_with_sleep(number)
-  counter
+  counter = number
+  while counter > 0
+    puts "#{number} SECOND(S)!"
+    counter -= 1 
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
   
 end 
