43e9bb7d0c980799fb7616ab239c080961282aff
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 19:41:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,18 @@
 #write your code here
 
 def countdown(number)
-  
+  counter = number
+  while counter > 0 
+  end
   "HAPPY NEW YEAR!"
 end
+
+counter = 0
+while counter < 20
+  puts "The current number is less than 20."
+  counter += 1
+end
+
 
 def countdown_with_sleep
 end 
